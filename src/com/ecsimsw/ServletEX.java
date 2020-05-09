0b6f1624bce7c9ce9aad19cea8c4d49423adb977
ecsimsw <ecsimsw@gmail.com>
2020-05-10 08:03:00 +0900

    servlet get /post

--- src/com/ecsimsw/ServletEX.java
+++ src/com/ecsimsw/ServletEX.java
@@ -14,38 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletEX
  */
-@WebServlet("/hello1")
+@WebServlet("/login")
 public class ServletEX extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
-	@PostConstruct
-	public void funcA() {
-		System.out.println("PostConstruct");
-	}
-	@Override
-	public void init() throws ServletException{
-		System.out.println("init");
-	}
-	@Override
-	public void destroy() {
-		System.out.println("destroy");
 
-	}
-	
-	@PreDestroy
-	public void funcB() {
-		System.out.println("PreDestroy");
-	}
-	
     public ServletEX() {
         super();
-        System.out.println("construct");
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("doGet");
+	
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		System.out.println("id : " + id + "  pw : "+pw);
 	}
 
 
